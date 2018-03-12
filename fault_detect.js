8e9d2b3fcb71659c5b1f281fca1508b9e91fcaeb
learningbeacon <anay.majee2014@vit.ac.in>
2018-03-12 09:46:31 +0530

    Updated fault detection block

--- fault_detect.js
+++ fault_detect.js
@@ -1,79 +1,40 @@
-const faultHandler = require("./fault_clear.js")
+/*
+  FAULT - DETECTION
+  This block is responsible for handling the various fault conditions based on fault type
+*/
+
 const fs = require('fs');
+const faultHandler = require("./fault_clear.js");
+const deviceFile = "./log/deviceParam.json";
+var deviceList = [];
 
-const deviceList = [];
-
-const detectOverVoltage = function(){
-    /*
-        OVER VOLTAGE FAULTS
-        They are non-instantaneos faults, so maximum fault voltage out of all is taken into consideration
-    */
-
-    var max = 0;
-    for(i = 0; i < deviceData.length; i++){
-        if(deviceData[i].volt_value > deviceData[i].vol_max && deviceData[i].volt_value > max){
-            max = deviceData[i].volt_value;
-            fault_device = deviceData[i].name;
-            fault_point = i;
-        }
-    }
-    if(max > 0){
-        console.log(".......Over Voltage Fault Detected near "+ fault_device +".......");
-        faultHandler.clearFault(deviceData[fault_point].name,deviceData.indexOf(fault_device));
-    }
-};
-
-const detectOverCurrent = function(){
+const detectFault = function(data){
 
     /*
         OVER CURRENT FAULTS
         They are non-instantaneos faults, so maximum fault current out of all is taken into consideration
     */
-    
     var max = 0;
-    for(i = 0; i < deviceData.length; i++){
-        if(deviceData[i].curr_value > deviceData[i].cur_max && deviceData[i].volt_value < 2.0){
-            fault_point = deviceData[i].name;
-            break;
+    for(i = 0; i < deviceList.length; i++){
+        if(data[deviceList[i]].curr_value > data[deviceList[i]].threshold){
+            fault_point = deviceList[i];
+            if(data[deviceList[i]].curr_value > max)
+                max = data[deviceList[i]].curr_value;
         }
     }
     if(max > 0){
-        console.log(".......Earth Fault Fault Detected near "+ fault_point +".......");
+        console.log(".......Fault Detected near "+ fault_point +".......");
         faultHandler.clearFault(fault_point,deviceData.indexOf(fault_point));
     }
-};
-
-const detectEarthFault = function(){
-    /*
-        EARTH FAULTS
-        They are instantaneos faults and needs to be cleared immediately, and one earth fault 
-        can cause other nodes to faulter also
-    */
-
-    var max = 0;
-    for(i = 0; i < deviceData.length; i++){
-        if(deviceData[i].curr_value > deviceData[i].cur_max && deviceData[i].curr_value > max){
-            max = deviceData[i].volt_value;
-            fault_point = deviceData[i].name;
-        }
-    }
-    if(max > 0){
-        console.log(".......Over Current Fault Detected near "+fault_point+".......");
-        faultHandler.clearFault(fault_point,deviceData.indexOf(fault_point));
-    }
-    //detectEarthFault();
 };
 
 setInterval(function(){
-    deviceData = JSON.parse(fs.readFileSync("./log/deviceParam.json"));
-    for(i = 0; i<deviceData.length; i++){
-        deviceList[i] = deviceData[i].name;
-    }
-    console.log(deviceList);
+    payload = fs.readFileSync(deviceFile,'utf8');
+    payload = JSON.parse(payload);
+    deviceList = Object.keys(payload);
     
-    detectOverVoltage();
+    console.log(deviceList);
+    detectFault(payload);
+},5000);
 
-    detectOverCurrent();
-
-    detectEarthFault();
-},10000)
+// call the default blocks at an interval of 5seconds
