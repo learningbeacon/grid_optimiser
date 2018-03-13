19572b1603d7e76328baa4490943b6211e033544
learningbeacon <anay.majee2014@vit.ac.in>
2018-03-13 12:14:01 +0530

    Updated fault detection module

--- fault_detect.js
+++ fault_detect.js
@@ -8,32 +8,32 @@ const faultHandler = require("./fault_clear.js");
 const deviceFile = "./log/deviceParam.json";
 var deviceList = [];
 
-const detectFault = function(data){
+const detectFault = function (data) {
 
     /*
         OVER CURRENT FAULTS
         They are non-instantaneos faults, so maximum fault current out of all is taken into consideration
     */
     var max = 0;
-    for(i = 0; i < deviceList.length; i++){
-        if(data[deviceList[i]].curr_value > data[deviceList[i]].threshold){
+    for (i = 0; i < deviceList.length; i++) {
+        if (data[deviceList[i]].curr_value > data[deviceList[i]].threshold) {
             fault_point = deviceList[i];
-            if(data[deviceList[i]].curr_value > max)
+            if (data[deviceList[i]].curr_value > max)
                 max = data[deviceList[i]].curr_value;
         }
     }
-    if(max > 0){
-        console.log(".......Fault Detected near "+ fault_point +".......");
+    if (max > 0) {
+        console.log(".......Fault Detected near " + fault_point + ".......");
         faultHandler.clearFault(deviceList.indexOf(fault_point));
     }
 };
 
-setInterval(function(){
-    payload1 = fs.readFileSync(deviceFile,'utf8');
+setInterval(function () {
+    payload1 = fs.readFileSync(deviceFile, 'utf8');
     payload1 = JSON.parse(payload1);
     deviceList = Object.keys(payload1);
-    
+
     detectFault(payload1);
-},4000);
+}, 4000);
 
 // call the default blocks at an interval of 5seconds
