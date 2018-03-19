7ec01b44ed89b38262faac732a1ea1f93b315908
learningbeacon <anay.majee2014@vit.ac.in>
2018-03-19 10:27:33 +0530

    Updated install script for Linux/UNIX env

--- INSTALL_script.sh
+++ INSTALL_script.sh
@@ -28,4 +28,40 @@ else
 	fi
 fi
 
+echo "...................................Checking for SERIAL PORT module.........................................."
+if [ -e ./node_modules/serialport/ ] 
+then
+        echo "\n\nSerial port library  already present... so no need to install again....\n\n"
+else
+        echo "$(tput setaf 1)$(tput bold)SERIAL PORT Module not present..... \n\n$(tput sgr0)"
+        read -p "Do you want to install sdk? (y/n)" input
+        if [ $input = 'y' ]
+        then
+                npm install serialport
+        else
+                echo "IGNORED... continuing with installation!"
+        fi
+fi
+
+echo "...................................Checking for WEATHER-JS module.........................................."
+if [ -e ./node_modules/weather-js/ ] 
+then
+        echo "\n\nWEATHER JS module  already present... so no need to install again....\n\n"
+else
+        echo "$(tput setaf 1)$(tput bold)WEATHER-JS Module not present..... \n\n$(tput sgr0)"
+        read -p "Do you want to install sdk? (y/n)" input
+        if [ $input = 'y' ]
+        then
+                npm install weather-js
+        else
+                echo "IGNORED... continuing with installation!"
+        fi
+fi
+
+if [ -e ./node_modules/weather-js/ ] && [ -e ./node_modules/serialport/ ] && [ -e ./node_modules/aws-iot-device-sdk/ ]
+then
+	echo "$(tput setaf 3) ALL MODULES INSTALLED SUCCESSFULLY$(tput sgr0)"
+else
+	echo "$(tput setaf 1) ALL MODULES NOT INSTALLED.... check dependency list in INSTALL_INSTRUCTIONS.txt$(tput sgr0)"
+fi
 
