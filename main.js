d691d5ae9131ec70d04b1c1c6f5c28b0122d9119
learningbeacon <anay.majee2014@vit.ac.in>
2018-03-20 11:32:08 +0530

    Added main block

---
+++ main.js
@@ -0,0 +1,14 @@
+/*
+    This is the main block acting as coordinator to all other blocks
+*/
+
+var serial_start = require("./serial_port.js");
+var fault_detect_start = require("./fault_detect.js");
+var load_balance_start = require("./loadBalance.js");
+var aws_upload_start = require("./aws_upload.js");
+
+/*
+
+END of code 
+
+*/
